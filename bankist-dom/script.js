a5d4e9ac5cc7b3ceec5d66c71c6a841ae586588d
Adam Vu <adam.vu@stem.com>
2022-08-15 16:19:02 +0700

    Add menu fade animation by using bind method

--- bankist-dom/script.js
+++ bankist-dom/script.js
@@ -10,6 +10,7 @@ const header = document.querySelector('.header');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 ///////////////////////////////////////
 // Modal window
@@ -98,3 +99,24 @@ tabsContainer.addEventListener('click', function (event) {
     .querySelector(`.operations__content--${clickedTab.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+///////////////////////////////////////
+// Menu fade animation
+
+const handleMouseHover = function (event) {
+  const link = event.target;
+  if (link.classList.contains('nav__link')) {
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleMouseHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleMouseHover.bind(1));
